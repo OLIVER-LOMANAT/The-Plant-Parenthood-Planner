305f59d483c21f0f95aea39aa9fa0ff74a7e6199
Oliver Lomanat <lomanatoliver@gmail.com>
2025-09-22 16:11:35 +0300

    Installing dependencies

--- backend/model.py
+++ backend/model.py
@@ -1,3 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy_serializer import SerializerMixin
 
 db = SQLAlchemy()
+
+class User(db.Model, SerializerMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column
+    
