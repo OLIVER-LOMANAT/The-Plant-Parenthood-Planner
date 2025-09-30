514f87ed59c4982c77975357e67919575dad29ad
Oliver Lomanat <lomanatoliver@gmail.com>
2025-09-30 09:23:54 +0300

    Adding authentication on checking login

--- client/src/pages/AddPlantPage.js
+++ client/src/pages/AddPlantPage.js
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import PlantForm from '../components/PlantForm';
 import { toast } from 'react-toastify';
 
-const AddPlantPage = () => {
+const AddPlantPage = ({ user }) => {
   const navigate = useNavigate();
 
   const handlePlantAdded = (plant) => {
@@ -11,10 +11,15 @@ const AddPlantPage = () => {
     navigate('/dashboard');
   };
 
+  if (!user) {
+    navigate('/login');
+    return null;
+  }
+
   return (
     <div className="min-h-screen bg-gray-50 py-8">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <PlantForm onPlantAdded={handlePlantAdded} />
+        <PlantForm onPlantAdded={handlePlantAdded} user={user} />
       </div>
     </div>
   );
